9983c8c1287b0daa1d1604faf800bc8f2b314743
mconour <michaelconour@gmail.com>
2023-11-02 22:12:28 -0700

    add firebase && add firebase hooks

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ import 'firebase/firestore';
 import 'firebase/auth';
 import 'firebase/analytics';
 
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  // your config
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+const analytics = firebase.analytics();
+
 
 function App() {
   return (
