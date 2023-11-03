f1da6cc82b6abbdf5cfbace4c08810c25967fd5b
mconour <michaelconour@gmail.com>
2023-11-02 23:00:52 -0700

    Use AuthState hook from react-firebase-hooks/auth to get the current user's authentication state && user is displayed either a ChatRoom component or a SignIn component based on whether they are authenticated && define the SignIn component && SignIn component allows the user to sign in with their Google account

--- src/App.js
+++ src/App.js
@@ -43,6 +43,20 @@ function App() {
   );
 }
 
+function SignIn() {
 
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+      <p>Do not violate the community guidelines or you will be banned for life!</p>
+    </>
+  )
+
+}
 
 export default App;
