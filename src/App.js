f9d841f19ccf74a8a081ce194e41a6d48a3a891e
mconour <michaelconour@gmail.com>
2023-11-04 13:15:12 -0700

    create signIn function component && signOut function component && chatRoom function component && chatMessage function component

--- src/App.js
+++ src/App.js
@@ -43,6 +43,60 @@ function App() {
   );
 }
 
+function SignIn() {
 
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return (
+    <>
+      <button onClick={signInWithGoogle}>Sign in with Google</button>
+      <p>Do not violate the community guidelines or you will be banned for life!</p>
+    </>
+  )
+
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+
+function ChatRoom() {
+
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+
+  const [messages] = useCollectionData(query, { idField: 'id' });
+
+
+
+
+  return (
+    <>
+      <div>
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+      </div>
+
+
+    </>)
+}
+
+
+function ChatMessage(props) {
+  const { text, uid } = props.message;
+
+
+
+  return (<>
+    <p>
+      {text}
+    </p>
+  </>)
+}
 
 export default App;
