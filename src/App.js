ca6f21508df279eb13e4b4cfa337b9e2f64fa625
mconour <michaelconour@gmail.com>
2023-11-05 23:57:09 -0800

    update all packages with latest

--- src/App.js
+++ src/App.js
@@ -10,14 +10,7 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
-  apiKey: "AIzaSyDkzi4EeO5RJLzwPrEFH9ZPptPPAD37GmQ",
-  authDomain: "superchat-9cac7.firebaseapp.com",
-  databaseURL: "https://superchat-9cac7-default-rtdb.firebaseio.com",
-  projectId: "superchat-9cac7",
-  storageBucket: "superchat-9cac7.appspot.com",
-  messagingSenderId: "587638261291",
-  appId: "1:587638261291:web:824443a38c49b84d8ccc6e",
-  measurementId: "G-BYPSFEQKH6"
+  // firebase configuration here
 })
 
 const auth = firebase.auth();
